a3c150d7fa021035358398b2303d09e5754a22e3
Your Name <you@example.com>
2023-04-05 16:23:46 +0300

    adaptive done, gif, hovers

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,38 @@
-const slider = document.getElementById("slider");
-const sliderValueProducts = document.getElementById("sliderValueProducts");
-const sliderValuePrice = document.getElementById("sliderValuePrice");
 
-sliderValueProducts.innerHTML = slider.value + " " +"шт.";
-sliderValuePrice.innerHTML = slider.value * 5 + " " + "₽";
 
-slider.oninput = function() {
-  sliderValueProducts.innerHTML = this.value + " " +"шт.";
-  sliderValuePrice.innerHTML = this.value * 5 + " " + "₽";
-}
+  const hero = document.querySelector('.section-hero');
+  const header = document.querySelector('.header-menu');
+  
+  const headerFixed = () => {
+    let scrollTop = window.scrollY;
+    let heroCenter = hero.offsetHeight / 2;
+    console.log(scrollTop);
+    console.log(heroCenter);
+    
+    if (scrollTop >= heroCenter) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+
+    headerFixed();
+
+    window.addEventListener('scroll', () => {
+      headerFixed();
+    });
+
+  };
+
+
+  const slider = document.getElementById("slider");
+  const sliderValueProducts = document.getElementById("sliderValueProducts");
+  const sliderValuePrice = document.getElementById("sliderValuePrice");
+
+  sliderValueProducts.innerHTML = slider.value + " " +"шт.";
+  sliderValuePrice.innerHTML = slider.value * 5 + " " + "₽";
+
+  slider.oninput = function() {
+    sliderValueProducts.innerHTML = this.value + " " +"шт.";
+    sliderValuePrice.innerHTML = this.value * 5 + " " + "₽";
+  }
+
