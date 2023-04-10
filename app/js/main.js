a314c931d50172d806ac6e77082282bc3e482d6e
Your Name <you@example.com>
2023-04-10 14:22:00 +0300

    animation refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,85 @@
-const scrollItems = document.querySelectorAll('.scroll-item');
-const scrollItemsOpacity = document.querySelectorAll('.scroll-opacity');
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('animation');
+//     }
+//   });
+// }
+// let observer = new IntersectionObserver(onEntry, { threshold: [0.5] });
+// let elements = document.querySelectorAll('.scroll-item');
+// for (let elm of elements) {
+//   observer.observe(elm);
+// }
 
-const scrollAnimation = () => {
-  let windowPoint = window.innerHeight / 1.2 + window.scrollY;
-  scrollItemsOpacity.forEach(el => {
-    let scrollOffset = el.offsetTop;
-    if (windowPoint >= scrollOffset) {
-      el.classList.add('animation-opacity');
-    } else {
-      el.classList.remove('animation-opacity');
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('animation2');
+//     }
+//   });
+// }
+// let observer2 = new IntersectionObserver(onEntry, { threshold: [0.5] });
+// let elements2 = document.querySelectorAll('.scroll-item2');
+// for (let elm of elements) {
+//   observer2.observe(elm);
+// }
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      if (change.target.classList.contains('scroll-item')) {
+        change.target.classList.add('animation');
+      } else if (change.target.classList.contains('scroll-dash')) {
+        change.target.classList.add('animation-dash');
+      }
     }
-  })
-};
-
-const scrollAnimationDash = () => {
-  let windowPoint = window.innerHeight / 1.2 + window.scrollY;
-  scrollItems.forEach(el => {
-    let scrollOffset = el.offsetTop;
-    if (windowPoint >= scrollOffset) {
-      el.classList.add('animation-dash');
-    } else {
-      el.classList.remove('animation-dash');
-    }
-  })
-};
-
-scrollAnimation();
-scrollAnimationDash();
-window.addEventListener('scroll', () => {
-  scrollAnimation();
-  scrollAnimationDash();
   });
+}
+  
+  let observer = new IntersectionObserver(onEntry, { threshold: [0.5] });
+  let elements = document.querySelectorAll('.scroll-item, .scroll-dash');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+
+
+
+
+
+// const scrollItems = document.querySelectorAll('.scroll-item');
+// const scrollItemsOpacity = document.querySelectorAll('.scroll-opacity');
+
+// const scrollAnimation = () => {
+//   let windowPoint = window.innerHeight / 1.2 + window.scrollY;
+//   scrollItemsOpacity.forEach(el => {
+//     let scrollOffset = el.offsetTop;
+//     if (windowPoint >= scrollOffset) {
+//       el.classList.add('animation-opacity');
+//     } else {
+//       el.classList.remove('animation-opacity');
+//     }
+//   })
+// };
+
+// const scrollAnimationDash = () => {
+//   let windowPoint = window.innerHeight / 1.2 + window.scrollY;
+//   scrollItems.forEach(el => {
+//     let scrollOffset = el.offsetTop;
+//     if (windowPoint >= scrollOffset) {
+//       el.classList.add('animation-dash');
+//     } else {
+//       el.classList.remove('animation-dash');
+//     }
+//   })
+// };
+
+// scrollAnimation();
+// scrollAnimationDash();
+// window.addEventListener('scroll', () => {
+//   scrollAnimation();
+//   scrollAnimationDash();
+//   });
 
 const slider = document.getElementById("slider");
 const sliderValueProducts = document.getElementById("sliderValueProducts");
@@ -43,24 +92,3 @@ slider.oninput = function() {
   sliderValueProducts.innerHTML = this.value + " " +"шт.";
   sliderValuePrice.innerHTML = this.value * 5 + " " + "₽";
 }
-
-// const hero = document.querySelector('.section-hero');
-// const header = document.querySelector('.header-menu'); 
-
-// const headerFixed = () => {
-//   let scrollTop = window.scrollY;
-//   let heroCenter = hero.offsetTop;
-
-//   if (scrollTop >= heroCenter) {
-//     header.classList.add('fixed');
-//     hero.style.marginTop = `${header.offsetHeight}px`
-//   } else {
-//     header.classList.remove('fixed');
-//     hero.style.marginTop = `0px`
-//   }
-// };
-// headerFixed();
-
-// window.addEventListener('scroll', () => {
-//   headerFixed();
-// });
