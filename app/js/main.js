cb6c33f09e3a5b77e5e1d40defbd366ddf84a6f8
Your Name <you@example.com>
2023-04-12 11:22:01 +0300

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,49 @@
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-      if (change.target.classList.contains('scroll-item')) {
-        change.target.classList.add('animation');
-      } else if (change.target.classList.contains('scroll-dash')) {
-        change.target.classList.add('animation-dash');
+function animateBlocks() {
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        if (change.target.classList.contains('scroll-item')) {
+          change.target.classList.add('animation');
+        } else if (change.target.classList.contains('scroll-dash')) {
+          change.target.classList.add('animation-dash');
+        }
       }
-    }
-  });
-}
-  
+    });
+  }
   let observer = new IntersectionObserver(onEntry, { threshold: [0.5] });
   let elements = document.querySelectorAll('.scroll-item, .scroll-dash');
   for (let elm of elements) {
     observer.observe(elm);
   }
-
-
-const slider = document.getElementById("slider");
-const sliderValueProducts = document.getElementById("sliderValueProducts");
-const sliderValuePrice = document.getElementById("sliderValuePrice");
-
-sliderValueProducts.innerHTML = slider.value + " " +"шт.";
-sliderValuePrice.innerHTML = slider.value * 5 + " " + "₽";
-
-slider.oninput = function() {
-  sliderValueProducts.innerHTML = this.value + " " +"шт.";
-  sliderValuePrice.innerHTML = this.value * 5 + " " + "₽";
 }
+
+
+function updateSliderValues() {
+
+  const slider = document.getElementById("slider");
+  const sliderValueProducts = document.getElementById("sliderValueProducts");
+  const sliderValuePrice = document.getElementById("sliderValuePrice");
+  
+  sliderValueProducts.innerHTML = slider.value + " " +"шт.";
+  sliderValuePrice.innerHTML = 1+" "+649+" "+305 + " " + "₽";
+  
+  slider.oninput = function() {
+    sliderValueProducts.innerHTML = this.value + " " + "шт.";
+    sliderValuePrice.innerHTML = (this.value * 5).toLocaleString() + " " + "₽";
+  }
+}
+
+updateSliderValues();
+animateBlocks();
+
+const burgerIcon = document.querySelector('.burger-icon');
+const burgerMenu = document.querySelector('.burger-menu');
+const headerMenu = document.querySelector('.header-menu');
+const body = document.body;
+
+burgerIcon.addEventListener('click', () => {
+  body.classList.toggle('stop-scroll')
+  burgerMenu.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+});
